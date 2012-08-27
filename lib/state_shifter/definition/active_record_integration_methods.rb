3f767e401eb2022fc74212dd475e842c6d95116d
Bruno Antunes <sardaukar.siet@gmail.com>
2012-08-27 15:48:07 +0200

    ~ DRYing it up

--- lib/state_shifter/definition/active_record_integration_methods.rb
+++ lib/state_shifter/definition/active_record_integration_methods.rb
@@ -4,18 +4,22 @@ module StateShifter
 
       class ::StateShifter::Definition::StatePersistenceAttributeNotPresent < RuntimeError; end 
 
-      def get_current_state
+      def check_attr_presence
         raise StatePersistenceAttributeNotPresent unless self.attribute_names.include? self.class.persist_attr_name.to_s
+      end
+
+      def get_current_state
+        check_attr_presence
         read_attribute self.class.persist_attr_name
       end
 
       def set_current_state value
-        raise StatePersistenceAttributeNotPresent unless self.attribute_names.include? self.class.persist_attr_name.to_s
+        check_attr_presence
         update_attribute self.class.persist_attr_name, value
       end
 
       def write_initial_state
-        raise StatePersistenceAttributeNotPresent unless self.attribute_names.include? self.class.persist_attr_name.to_s
+        check_attr_presence
         write_attribute self.class.persist_attr_name, self.class.state_machine_definition.initial_state.name.to_sym
       end
 
