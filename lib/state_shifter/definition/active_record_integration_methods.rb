4f3342d0a95794b461788299f5e909115fe915f4
Bruno Antunes <sardaukar.siet@gmail.com>
2012-08-28 12:18:04 +0200

    ! no need to check attr presence on initial write (fixes Rails validation errors being shown as StateShifter ones)

--- lib/state_shifter/definition/active_record_integration_methods.rb
+++ lib/state_shifter/definition/active_record_integration_methods.rb
@@ -19,7 +19,6 @@ module StateShifter
       end
 
       def write_initial_state
-        check_attr_presence
         write_attribute self.class.persist_attr_name, self.class.state_machine_definition.initial_state.name.to_sym
       end
 
