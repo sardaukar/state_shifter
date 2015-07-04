f99ff756506412c6afb0447b445cfbc3d9e7bb39
Bruno Antunes <sardaukar.siet@gmail.com>
2015-07-04 09:25:25 +0100

    quelches spec deprecation warnings

--- spec/state_shifter_spec.rb
+++ spec/state_shifter_spec.rb
@@ -12,94 +12,94 @@ shared_examples_for 'a simple state machine' do
   end
 
   it 'should have all states and events defined' do
-    @state_machine.state_names.should == [:new, :awaiting_review, :being_reviewed, :accepted, :rejected]
-    @state_machine.event_names.should == [:submit, :review, :accept, :reject]
+    expect(@state_machine.state_names).to eq [:new, :awaiting_review, :being_reviewed, :accepted, :rejected]
+    expect(@state_machine.event_names).to eq [:submit, :review, :accept, :reject]
   end
 
   it 'should have the proper initial state set' do
-    @state_machine.initial_state.should == :new
-    @state_machine.current_state.should == :new
+    expect(@state_machine.initial_state).to eq :new
+    expect(@state_machine.current_state).to eq :new
 
-    @state_machine.current_state_def.initial?.should be_true
+    expect(@state_machine.current_state_def.initial?).to be_truthy
 
     @state_machine.submit
-    @state_machine.initial_state.should == :new
+    expect(@state_machine.initial_state).to eq :new
 
-    @state_machine.current_state.should == :awaiting_review
+    expect(@state_machine.current_state).to eq :awaiting_review
   end
 
   it 'should transit between states properly' do
-    @state_machine.current_state.should == :new
+    expect(@state_machine.current_state).to eq :new
     @state_machine.submit
-    @state_machine.current_state.should == :awaiting_review
+    expect(@state_machine.current_state).to eq :awaiting_review
 
     @state_machine.review!
-    @state_machine.current_state.should == :being_reviewed
+    expect(@state_machine.current_state).to eq :being_reviewed
   end
 
   it 'should know the current state' do
-    @state_machine.current_state.should == :new
+    expect(@state_machine.current_state).to eq :new
 
-    @state_machine.new?.should be_true
-    @state_machine.accepted?.should be_false
+    expect(@state_machine.new?).to be_truthy
+    expect(@state_machine.accepted?).to be_falsey
 
     @state_machine.submit
 
-    @state_machine.awaiting_review?.should be_true
-    @state_machine.new?.should be_false
+    expect(@state_machine.awaiting_review?).to be_truthy
+    expect(@state_machine.new?).to be_falsey
   end
 
   it 'should respect guard statements' do
-    @state_machine.current_state.should == :new
+    expect(@state_machine.current_state).to eq :new
     @state_machine.submit
     @state_machine.review
 
-    @state_machine.reject.should_not be_true
-    lambda { @state_machine.reject!}.should raise_error(StateShifter::GuardNotSatisfied, 'bad_article?')
+    expect(@state_machine.reject).to_not be_truthy
+    expect(lambda { @state_machine.reject!}).to raise_error(StateShifter::GuardNotSatisfied, 'bad_article?')
 
-    @state_machine.accept.should be_true
-    @state_machine.current_state.should == :accepted
+    expect(@state_machine.accept).to be_truthy
+    expect(@state_machine.current_state).to eq :accepted
   end
 
   it 'should know if a transition is possible' do
-    @state_machine.current_state.should == :new
-    @state_machine.can_submit?.should be_true
+    expect(@state_machine.current_state).to eq :new
+    expect(@state_machine.can_submit?).to be_truthy
 
     @state_machine.submit
 
-    @state_machine.can_review?.should be_true
+    expect(@state_machine.can_review?).to be_truthy
 
     @state_machine.review
 
-    @state_machine.can_accept?.should be_true
-    @state_machine.can_reject?.should be_false
+    expect(@state_machine.can_accept?).to be_truthy
+    expect(@state_machine.can_reject?).to be_falsey
   end
 
   it 'should properly indicate next states' do
-    @state_machine.current_state.should == :new
-    @state_machine.next_states.should == [:awaiting_review]
-    @state_machine.transitionable_states.should == [:awaiting_review]
+    expect(@state_machine.current_state).to eq :new
+    expect(@state_machine.next_states).to eq [:awaiting_review]
+    expect(@state_machine.transitionable_states).to eq [:awaiting_review]
 
-    @state_machine.current_state_def.final?.should be_false
+    expect(@state_machine.current_state_def.final?).to be_falsey
 
     @state_machine.submit!
-    @state_machine.next_states.should == [:being_reviewed]
-    @state_machine.transitionable_states.should == [:being_reviewed]
+    expect(@state_machine.next_states).to eq [:being_reviewed]
+    expect(@state_machine.transitionable_states).to eq [:being_reviewed]
 
     @state_machine.review!
-    @state_machine.next_states.should == [:accepted, :rejected]
-    @state_machine.transitionable_states.should == [:accepted]
+    expect(@state_machine.next_states).to eq [:accepted, :rejected]
+    expect(@state_machine.transitionable_states).to eq [:accepted]
 
     @state_machine.accept
-    @state_machine.next_states.should == []
-    @state_machine.transitionable_states.should == []
+    expect(@state_machine.next_states).to eq []
+    expect(@state_machine.transitionable_states).to eq []
 
-    @state_machine.current_state_def.final?.should be_true
+    expect(@state_machine.current_state_def.final?).to be_truthy
   end
 
   it 'should respect proper transition precedence' do
-    @state_machine.current_state.should == :new
-    lambda { @state_machine.review! }.should raise_error(StateShifter::TransitionHalted, 'you cannot transition from new via review')
+    expect(@state_machine.current_state).to eq :new
+    expect(lambda { @state_machine.review! }).to raise_error(StateShifter::TransitionHalted, 'you cannot transition from new via review')
   end
 
 end
@@ -113,15 +113,15 @@ end
 describe 'Malformed state machines' do
 
   it 'should complain about redifining states' do
-    lambda { require_relative '../examples/malformed_states' }.should raise_error(StateShifter::RedifiningState, 'new')
+    expect(lambda { require_relative '../examples/malformed_states' }).to raise_error(StateShifter::RedifiningState, 'new')
   end
 
   it 'should complain about redifining events' do
-    lambda { require_relative '../examples/malformed_events' }.should raise_error(StateShifter::RedifiningEvent, 'submit')
+    expect(lambda { require_relative '../examples/malformed_events' }).to raise_error(StateShifter::RedifiningEvent, 'submit')
   end
 
   it 'should complain when the persistence attribute is set after the state machine definition' do
-    lambda { require_relative '../examples/malformed_persistence' }.should raise_error(StateShifter::PersistenceAttributeAlreadyDefined)
+    expect(lambda { require_relative '../examples/malformed_persistence' }).to raise_error(StateShifter::PersistenceAttributeAlreadyDefined)
   end
 
 end
@@ -133,32 +133,30 @@ describe 'Advanced state machine functionality' do
   end
 
   it 'should complain about undefined guards' do
-    lambda { @advanced.can_start_date_reached?}.should raise_error(StateShifter::GuardMethodUndefined, 'start_date_reached?')
+    expect(lambda { @advanced.can_start_date_reached?}).to raise_error(StateShifter::GuardMethodUndefined, 'start_date_reached?')
   end
 
   it 'should call looping event callbacks' do
-    @advanced.stub!(:handle_start_date_changed)
-
-    @advanced.should_receive(:handle_start_date_changed)
+    expect(@advanced).to receive(:handle_start_date_changed)
     @advanced.start_date_changed
   end
 
   it 'state on_entry callbacks should work' do
     # block
-    @advanced.should_receive(:running_entry).with(:initialized, :forced_start).and_return(nil)
+    expect(@advanced).to receive(:running_entry).with(:initialized, :forced_start).and_return(nil)
     @advanced.forced_start
 
     # method name only
-    @advanced.should_receive(:send_notification_to_organizers).with(no_args())
+    expect(@advanced).to receive(:send_notification_to_organizers).with(no_args())
     @advanced.deadline_reached!
   end
 
   it 'state on_entry callbacks with an argument should work' do
     @advanced.forced_start
-    @advanced.stub!(:entries_deadline_reached_without_approvals?).and_return(true)
+    allow(@advanced).to receive(:entries_deadline_reached_without_approvals?).and_return(true)
 
     # method name only with args
-    @advanced.should_receive(:send_notification).with(:pending_users)
+    expect(@advanced).to receive(:send_notification).with(:pending_users)
     @advanced.deadline_reached_without_approvals!
   end
 
@@ -166,18 +164,18 @@ describe 'Advanced state machine functionality' do
     @advanced.forced_start
 
     # method name only with args
-    @advanced.should_receive(:send_notification).with([:stakeholders, :organizers])
+    expect(@advanced).to receive(:send_notification).with([:stakeholders, :organizers])
     @advanced.abort_running_contest!
   end
 
   it 'the on_transition callback should work' do
-    @advanced.should_receive(:benchmark).with(:initialized, :running, :forced_start!, an_instance_of(Float)).and_return(nil)
+    expect(@advanced).to receive(:benchmark).with(:initialized, :running, :forced_start!, an_instance_of(Float)).and_return(nil)
     @advanced.forced_start!
   end
 
   it 'the on_transition callback should work in the proper order' do
-    @advanced.should_receive(:benchmark).ordered.with(:initialized, :preparing, :event_associated!, an_instance_of(Float)).and_return(nil)
-    @advanced.should_receive(:benchmark).ordered.with(:preparing, :running, :all_done!, an_instance_of(Float)).and_return(nil)
+    expect(@advanced).to receive(:benchmark).ordered.with(:initialized, :preparing, :event_associated!, an_instance_of(Float)).and_return(nil)
+    expect(@advanced).to receive(:benchmark).ordered.with(:preparing, :running, :all_done!, an_instance_of(Float)).and_return(nil)
     @advanced.event_associated!
   end
 
@@ -207,37 +205,37 @@ describe Review do
     state_machine = described_class.new
     state_machine.save
 
-    state_machine.current_state.should == :new
+    expect(state_machine.current_state).to eq :new
     state_machine.update_attribute(:current_state, "new")
 
-    state_machine.new?.should be_true
+    expect(state_machine.new?).to be_truthy
 
     state_machine.submit
-    state_machine.initial_state.should == :new
+    expect(state_machine.initial_state).to eq :new
 
-    state_machine.current_state.should == :awaiting_review
+    expect(state_machine.current_state).to eq :awaiting_review
   end
 
   it "should have tag methods" do
-    described_class.reviewable_states.should eq([:new, :awaiting_review, :being_reviewed])
+    expect(described_class.reviewable_states).to eq([:new, :awaiting_review, :being_reviewed])
 
     state_machine = described_class.new
     state_machine.save
 
-    state_machine.reviewable?.should be_true
-    state_machine.processing?.should be_false
+    expect(state_machine.reviewable?).to be_truthy
+    expect(state_machine.processing?).to be_falsey
 
     state_machine.submit
-    state_machine.reviewable?.should be_true
-    state_machine.processing?.should be_true
+    expect(state_machine.reviewable?).to be_truthy
+    expect(state_machine.processing?).to be_truthy
 
     state_machine.review!
-    state_machine.reviewable?.should be_true
-    state_machine.processing?.should be_true
+    expect(state_machine.reviewable?).to be_truthy
+    expect(state_machine.processing?).to be_truthy
 
     state_machine.accept!
-    state_machine.reviewable?.should be_false
-    state_machine.processing?.should be_false
+    expect(state_machine.reviewable?).to be_falsey
+    expect(state_machine.processing?).to be_falsey
   end
 
   it "should have state scopes" do
@@ -245,8 +243,8 @@ describe Review do
     state_machine.save
     state_machine.submit
 
-    described_class.awaiting_review.size.should eq(1)
-    described_class.reviewable.size.should eq(1)
+    expect(described_class.awaiting_review.size).to eq(1)
+    expect(described_class.reviewable.size).to eq(1)
   end
 
 end
@@ -271,15 +269,15 @@ describe ReviewCustomPersistence do
     state_machine = described_class.new
     state_machine.save
 
-    state_machine.current_state.should == :new
+    expect(state_machine.current_state).to eq :new
     state_machine.update_attribute(:stamp, "new")
 
-    state_machine.new?.should be_true
+    expect(state_machine.new?).to be_truthy
 
     state_machine.submit
-    state_machine.initial_state.should == :new
+    expect(state_machine.initial_state).to eq :new
 
-    state_machine.current_state.should == :awaiting_review
+    expect(state_machine.current_state).to eq :awaiting_review
   end
 
 end
@@ -301,7 +299,7 @@ describe 'Malformed persistence definition' do
   it 'should complain when the persist attribute is not present' do
     require_relative '../examples/missing_persistence'
     review_custom_persistence = MissingPersistence.new
-    lambda { review_custom_persistence.save }.should raise_error(StateShifter::Definition::StatePersistenceAttributeNotPresent)
+    expect(lambda { review_custom_persistence.save }).to raise_error(StateShifter::Definition::StatePersistenceAttributeNotPresent)
   end
 
 end
